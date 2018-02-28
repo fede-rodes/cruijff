4c8a3cfc4eb1f646b3e0ce72d376aeefeec4693c
Tom Klaver <tomklav@gmail.com>
2018-02-28 09:08:14 +0100

    styling slider

--- App/Components/Slider.js
+++ App/Components/Slider.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Animated, PanResponder, View } from 'react-native'
+import { Animated, PanResponder, View, StyleSheet } from 'react-native'
 import Text from './Text'
+import Colors from '../Themes/Colors'
+
+const handleSize = 25
 
 export default class extends React.Component {
   constructor () {
@@ -18,52 +21,86 @@ export default class extends React.Component {
       },
       onPanResponderMove: Animated.event([null, { dx: this.state.pan }]),
       onPanResponderRelease: (e, { vx, vy }) => {
+        console.log('release')
+        console.log(this.state.pan)
         this.state.pan.flattenOffset()
-        // clamp the value between 0 and width
+        // clamp the value between 0 and width (minus handleWidth)
         this.state.pan.setValue(
-          Math.max(Math.min(this.state.width, this.state.pan._value), 0)
+          Math.max(Math.min(this.maxX(), this.state.pan._value), this.minX())
+        )
+        console.log(
+          (this.state.pan._value - handleSize / 2) /
+            (this.state.width - handleSize)
         )
       }
     })
   }
 
+  minX () {
+    return handleSize / 2
+  }
+  maxX () {
+    return this.state.width - handleSize / 2
+  }
+
   render () {
     const x = this.state.pan.interpolate({
-      inputRange: [0, this.state.width],
-      outputRange: [0, this.state.width],
+      inputRange: [this.minX(), this.maxX()],
+      outputRange: [this.minX(), this.maxX()],
       extrapolate: 'clamp'
     })
 
+    console.log(x, this.minX(), this.maxX())
+
     return (
       <View
-        onLayout={a => this.setState({ width: a.nativeEvent.layout.width })}
-        style={{ flex: 1, backgroundColor: 'red' }}
+        onLayout={e => this.setState({ width: e.nativeEvent.layout.width })}
+        style={{ flex: 1, justifyContent: 'center' }}
       >
-        <View
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            backgroundColor: 'green'
-          }}
-        />
+        <View style={style.scaleContainer}>
+          <View style={style.scale} />
+        </View>
         <Animated.View
           {...this.panResponder.panHandlers}
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'center',
-            transform: [{ translateX: x }],
-            backgroundColor: 'transparent',
-            left: -50,
-            width: 100
-          }}
+          style={[style.handleContainer, { transform: [{ translateX: x }] }]}
         >
-          <View style={{ width: 10, backgroundColor: 'blue' }} />
+          <View style={style.handle} />
         </Animated.View>
       </View>
     )
   }
 }
+
+const style = StyleSheet.create({
+  scaleContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderLeftColor: Colors.actionYellow,
+    borderLeftWidth: 5,
+    borderRightColor: Colors.actionYellow,
+    borderRightWidth: 5
+  },
+  scale: {
+    flex: 1,
+    height: 5,
+    backgroundColor: Colors.actionYellow
+  },
+  handleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: -50,
+    width: 100
+  },
+  handle: {
+    width: handleSize,
+    height: handleSize,
+    backgroundColor: Colors.black,
+    borderRadius: handleSize
+  }
+})
